73abc8ab2cc1a5b1e6ebd40aa90fead4f32efe8c
sagar devkota <41661872+Sagar1998a@users.noreply.github.com>
2018-09-24 16:07:12 +1200

    Add files via upload

---
+++ class-exercise/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Animal
+    {
+        public int animal { get; set; }
+        string name, sound;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Sound
+        {
+            get { return sound; }
+            set { sound = value; }
+        }
+        public Animal(string _name, string _sound)
+        {
+            Name = _name;
+            Sound = _sound;
+        }
+
+        public string AnimalSays()
+        {
+            return "The " + Name + " says " + Sound;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter the animals type ie. Cow, dog, cat: ");
+            string animalType = Console.ReadLine();
+            Console.Write("Please enter the sound the animal makes: ");
+            string animalSound = Console.ReadLine();
+            Animal animal1 = new Animal(animalType, animalSound);
+            Console.WriteLine(animal1.AnimalSays());
+        }
+    }
+}
+
+    
